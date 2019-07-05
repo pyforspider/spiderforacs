e5717305299ec3ff92f2f6bc20a6a11867a6d99c
shaojian <1303292392@qq.com>
2019-07-05 19:24:00 +0800

    Now all pdf files showed in different pages of the website can be download, and names of them is correct.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spiderforacs
+This script is used for downloading PDF files from ACS publication.
+User guide: Input a keyword. If keywords, use "+" to connect them.
--- spiderforacs.py
+++ spiderforacs.py
@@ -4,7 +4,7 @@ import os
 from lxml import etree
 
 
-def get_one_page(url,headers):
+def get_html_text(url, headers):
 	try:
 		r = requests.get(url, headers=headers)
 		r.raise_for_status()
@@ -14,25 +14,15 @@ def get_one_page(url,headers):
 		return "Failed"
 
 
-def parse_one_page(html):
+def parse_pdf_link(html):
 	pattern = re.compile('.*?title="PDF".*?href="(.*?)"', re.S)
 	pdf_links = re.findall(pattern, html)
-	return pdf_links
+	pdf_intact_links = []
+	for pdf_link in pdf_links:
+		pdf_intact_link = 'https://pubs.acs.org' + pdf_link
+		pdf_intact_links.append(pdf_intact_link)
+	return pdf_intact_links
 
-
-# def parse_names(html, kw):
-	# This section has a problem with keyword= MOF-5
-	# pattern2 = re.compile('.*?class="issue-item_title".*?href=.*?>(.*?)<span', re.S)
-	# pattern3 = re.compile('.*?class="single_highlight_class">(.*?)</span>.*?</a></h5><h4>', re.S)
-	# pattern4 = re.compile('.*?class="issue-item_title".*?class="single_highlight_class".*?</span>(.*?)</a></h5><h4></h4></span><ul.class="issue-item_loa"', re.S)
-	# pdfnames2 = re.findall(pattern2, html)
-	# pdfnames3 = re.findall(pattern3, html)
-	# pdfnames4 = re.findall(pattern4, html)
-	# pdfnames = []
-	# for i in range(len(pdfnames2)):
-	# 	a = pdfnames2[i] + pdfnames3[i] + pdfnames4[i]
-	# 	pdfnames.append(a)
-	# return pdfnames
 
 def parser_names(html):
 	selector = etree.HTML(html)
@@ -42,8 +32,15 @@ def parser_names(html):
 		'''This small black point '.' costs me hours.'''
 		pdf_name = ''
 		pdf_name_splits = h5.xpath('.//text()')
-		for i in pdf_name_splits:
-			pdf_name += i
+		for split in pdf_name_splits:
+			pdf_name += split
+		pdfname_str_lis = list(pdf_name)
+		number = 0
+		for char in pdfname_str_lis:
+			if char in ['<', '>', '/', '\\', '|', ':', '"', '*', '?']:
+				pdfname_str_lis.pop(number)
+			number += 1
+		pdf_name = ''.join(pdfname_str_lis)
 		pdf_name_list.append(pdf_name)
 	return pdf_name_list
 
@@ -52,7 +49,7 @@ def save_pdf(pdf_intact_links, big_path, pdf_names, i):
 	count = 0
 	for pdf_intact_link in pdf_intact_links:
 		try:
-			print("Acquiring pdf file " + pdf_names[count] + ".pdf")
+			print("Acquiring PDF file: " + pdf_names[count][0:60] + '...' + ".pdf")
 			r = requests.get(pdf_intact_link)
 			file_path = big_path + os.path.sep + '{file_name}.{file_suffix}'.format(
 				file_name=str(count+i*20) + ' ' + pdf_names[count],
@@ -60,8 +57,9 @@ def save_pdf(pdf_intact_links, big_path, pdf_names, i):
 			with open(file_path, 'wb') as f:
 				f.write(r.content)
 				count = count + 1
-		except:
-			print('Fail to download file ' + pdf_names[count])
+				print('Success to download PDF file.')
+		except: # FileNotFoundError or OSError
+			print('Fail to download PDF file.')
 			count = count + 1
 
 
@@ -71,27 +69,20 @@ def main(kw, page_number):
 	big_path = 'Literature for ' + kw + os.path.sep
 	if not os.path.exists(big_path):
 		os.makedirs(big_path)
-
 	for i in range(page_number):
 		url_page = url + "&startPage=" + str(i) + "&pageSize=20"
-		html = get_one_page(url_page, headers)
-		pdf_links = parse_one_page(html)
-		pdf_intact_links = []
-		for pdf_link in pdf_links:
-			pdf_intact_link = 'https://pubs.acs.org' + pdf_link
-			pdf_intact_links.append(pdf_intact_link)
+		html = get_html_text(url_page, headers)
+		pdf_intact_links = parse_pdf_link(html)
 		pdf_names = parser_names(html)
 		save_pdf(pdf_intact_links, big_path, pdf_names, i)
 
 
 if __name__ == '__main__':
-	kw = 'BODIPY'
 	total_page = 10
-	kw = input ('Key Word: ')
-	main(kw, total_page)
+	keyword = input('Key Word: ')
+	main(keyword, total_page)
+
 
 #   PQuery can do like this:
 #   wrap.find('p').remove()
 #   print(wrap.text())
-	
-
